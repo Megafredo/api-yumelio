ee482bae60b86ffe1414077430a9a921d157f062
Yumicode <75518128+helene-nguyen@users.noreply.github.com>
2022-11-19 14:57:26 +0100

    :dizzy: delete : unused validation ajv file

--- dist/app/middlewares/getRefreshToken.js
+++ dist/app/middlewares/getRefreshToken.js
@@ -6,11 +6,11 @@ function getRefreshToken(req, res, next) {
     try {
         const authHeader = req.headers['authorization'];
         if (authHeader === undefined)
-            throw new ErrorApi('Aucun token trouvé', req, res, 400);
+            throw new ErrorApi('No token found !', req, res, 400);
         let refreshToken = authHeader.split(' ')[1];
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
             if (err) {
-                throw new ErrorApi('Le token est invalide !', req, res, 403);
+                throw new ErrorApi('The token is invalid!', req, res, 403);
             }
             req.session.refreshToken = [];
             req.user = user.user;
